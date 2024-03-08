8faf9ed9070c4daf714f9388887e43a0020bf4ee
Keira Krieger <keira.f.krieger@gmail.com>
2024-03-07 18:16:38 -0800

    Add project files.

---
+++ Flashcards/Models/FlashCards.cs
@@ -0,0 +1,10 @@
+﻿public class FlashCards
+{
+    public int Id { get; set; }
+
+    public string? CardFront { get; set; }
+
+    public string? CardBack { get; set;}
+
+    public int StackId { get; set; }
+}
---
+++ Flashcards/Models/StudySession.cs
@@ -0,0 +1,10 @@
+﻿public class StudySession
+{
+    public int Id { get; set; }
+
+    public DateTime SessionDate { get; set; }
+
+    public int SessionScore { get; set; }
+
+    public int StackId { get; set; }
+}
